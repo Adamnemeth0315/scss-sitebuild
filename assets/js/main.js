706191a6678b2a635ed0bc08a801948ba2ee375e
Adam <adam.nemeth.0315@gmail.com>
2021-01-16 13:14:53 +0100

    Még a js-en kell finomítani.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,30 @@
 const navbar = document.querySelector('.nav');
 const sticky = navbar.offsetTop;
 const link = document.querySelectorAll('.nav__link');
+const openButtonPrivacy = document.querySelector('.modal__open-privacy');
+const openButtonTerms = document.querySelector('.modal__open-terms');
+const openButtonFaq = document.querySelector('.modal__open-faq');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+const closeButton = document.querySelector('.closebtn');
 
+//Modal
+const giveClassFunction = (ev) => {
+    ev.preventDefault();
+    modal.setAttribute('class', 'modal showModal');
+}
+const takeAwayClassFunction = () => modal.setAttribute('class', 'modal');
 
+openButtonPrivacy.addEventListener('click', giveClassFunction);
+openButtonTerms.addEventListener('click', giveClassFunction);
+openButtonFaq.addEventListener('click', giveClassFunction);
+
+closeModal.addEventListener('click', takeAwayClassFunction);
+closeButton.addEventListener('click', takeAwayClassFunction);
+
+modal.addEventListener('click', takeAwayClassFunction);
+
+//Navbar
 function stickyNavBarFunction() {
     if (window.pageYOffset > sticky) {
         navbar.classList.add("sticky");
